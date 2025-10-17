feae484831c4ed9bc45c3951a6d7d32c154d67d3
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2025-10-17 21:39:01 +0530

    fix: Add Coil dependency to fix build failure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,6 +94,9 @@ dependencies {
     implementation("androidx.datastore:datastore-preferences:1.1.7")
     implementation("androidx.datastore:datastore-core:1.1.7")
 
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.7.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
