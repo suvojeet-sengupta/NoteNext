178b570b0c8e42c814418100cd5e1713c70e066b
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2025-10-27 18:33:50 +0530

    fix: Revert image contentScale to Fit to prevent cropping

--- app/src/main/java/com/suvojeet/notenext/ui/add_edit_note/AddEditNoteScreen.kt
+++ app/src/main/java/com/suvojeet/notenext/ui/add_edit_note/AddEditNoteScreen.kt
@@ -211,7 +211,7 @@ fun AddEditNoteScreen(
                                                     .fillMaxWidth()
                                                     .height(200.dp)
                                                     .padding(8.dp),
-                                                contentScale = ContentScale.None
+                                                contentScale = ContentScale.Fit
                                             )
                                         }
                                         else -> {
