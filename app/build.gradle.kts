96ffd82bc01617ff7fed7122edbab8f799fff2ee
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2025-11-02 21:35:52 +0530

    chore: Increment versionCode to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.suvojeet.notenext"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
