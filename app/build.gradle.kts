503a7ad0082be690a365ce1cc6e52d48469a4509
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2025-11-03 21:51:34 +0530

    feat: Increment versionCode to 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.suvojeet.notenext"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
+        versionCode = 5
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
