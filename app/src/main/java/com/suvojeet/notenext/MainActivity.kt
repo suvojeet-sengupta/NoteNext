bbf5832035b39458ecb481d27051a0a069014ff3
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2025-11-18 16:34:52 +0530

    Refactor MainActivity to remove runBlocking and use ComponentActivity

--- app/src/main/java/com/suvojeet/notenext/MainActivity.kt
+++ app/src/main/java/com/suvojeet/notenext/MainActivity.kt
@@ -28,49 +28,40 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import com.suvojeet.notenext.ui.lock.LockScreen
 import androidx.core.view.WindowCompat
-import androidx.fragment.app.FragmentActivity
-
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
+import androidx.activity.ComponentActivity
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.getValue
-import com.suvojeet.notenext.ui.setup.SetupScreen
-
-import androidx.compose.ui.Modifier
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.launch
 
-class MainActivity : FragmentActivity() {
+class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val settingsRepository = SettingsRepository(this)
-        val languageCode = runBlocking { settingsRepository.language.first() }
-        val locale = Locale(languageCode)
-        Locale.setDefault(locale)
-        val config = Configuration(resources.configuration)
-        config.setLocale(locale)
-        resources.updateConfiguration(config, resources.displayMetrics)
 
         enableEdgeToEdge()
         val database = NoteDatabase.getDatabase(this)
         val linkPreviewRepository = LinkPreviewRepository()
         val factory = ViewModelFactory(database.noteDao(), database.labelDao(), database.projectDao(), linkPreviewRepository, application)
-        // val settingsRepository = SettingsRepository(this) // Already initialized above
 
         val startNoteId = intent.getIntExtra("NOTE_ID", -1)
         setContent {
+            val languageCode by settingsRepository.language.collectAsState(initial = "en")
+            LaunchedEffect(languageCode) {
+                val locale = Locale(languageCode)
+                Locale.setDefault(locale)
+                val config = Configuration(resources.configuration)
+                config.setLocale(locale)
+                resources.updateConfiguration(config, resources.displayMetrics)
+            }
+
             val windowSizeClass = calculateWindowSizeClass(this)
             val themeMode by settingsRepository.themeMode.collectAsState(initial = ThemeMode.SYSTEM)
             val shapeFamily by settingsRepository.shapeFamily.collectAsState(initial = ShapeFamily.EXPRESSIVE)
 
             var enableAppLockLoaded by remember { mutableStateOf<Boolean?>(null) }
-            var isSetupCompleteLoaded by remember { mutableStateOf<Boolean?>(null) }
+            var isSetupCompleteLoaded by remember { mutableState of<Boolean?>(null) }
 
             LaunchedEffect(Unit) {
                 settingsRepository.enableAppLock.collect { enableAppLockLoaded = it }
