111744673a1518c87adbae7134db082b82bf049e
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2025-12-22 00:29:45 +0530

    Fix: Remove restricted CLOSE_SYSTEM_DIALOGS broadcast in TileService

--- app/src/main/java/com/suvojeet/notenext/service/QuickNoteTileService.kt
+++ app/src/main/java/com/suvojeet/notenext/service/QuickNoteTileService.kt
@@ -9,10 +9,6 @@ class QuickNoteTileService : TileService() {
     override fun onClick() {
         super.onClick()
         
-        // Collapse the status bar/notification drawer
-        val closeIntent = Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)
-        sendBroadcast(closeIntent)
-
         val intent = Intent(this, MainActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             action = "com.suvojeet.notenext.ACTION_CREATE_NOTE"
