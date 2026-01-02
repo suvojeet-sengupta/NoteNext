e63d80d57b49f052409a747ec40a13f81deef47c
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2026-01-02 18:46:47 +0530

    Build: Update Kotlin to 2.3.0 and KSP to 2.3.0-1.0.32

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.13.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
-    id("com.google.devtools.ksp") version "2.2.20-2.0.3" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
+    id("com.google.devtools.ksp") version "2.3.0-1.0.32" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.0" apply false
     id("com.google.dagger.hilt.android") version "2.57.1" apply false
 }
