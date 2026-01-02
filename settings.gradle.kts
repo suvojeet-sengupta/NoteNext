2c6c110a184d9f0892e595d4716c0606d1fa4f0d
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2026-01-02 19:50:41 +0530

    Build: Use explicit repo1.maven.org to avoid 403 Forbidden errors

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,8 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        mavenCentral()
+        maven { url = uri("https://repo1.maven.org/maven2") }
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
