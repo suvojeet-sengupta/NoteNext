2d6ede81abeb5e85868d4612dc3d7aedc572620d
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2026-01-02 18:50:10 +0530

    Build: Fix KSP version to 2.3.0 (matching Kotlin 2.3.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@
 plugins {
     id("com.android.application") version "8.13.0" apply false
     id("org.jetbrains.kotlin.android") version "2.3.0" apply false
-    id("com.google.devtools.ksp") version "2.3.0-1.0.32" apply false
+    id("com.google.devtools.ksp") version "2.3.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.0" apply false
     id("com.google.dagger.hilt.android") version "2.57.1" apply false
 }
